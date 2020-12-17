f0fb5f9f7bda449a7147e4edc19bb1840ffad50f
chinguyen21 <nguyen.thimai@northeastern.edu>
2020-12-16 23:50:42 -0500

    commit tree ee2d27ec90fd447ad6c6559ca630872cc40749f3

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#homepage'
+  resources :users
+  
+  get '/signup', to: 'users#new', as: "new_signup"
+  post '/signup', to: 'users#create'
+  get '/users/new', to: 'sessions#new'
+  post '/users/new', to: 'sessions#create'
+
 end
